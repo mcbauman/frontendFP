bb4b9132017b6573099ffb9706bd0db393d36ee9
mcbauman <m.c.baumann@icloud.com>
2022-06-09 10:05:19 +0200

    color and Picture upload

--- src/Routes/Profile.jsx
+++ src/Routes/Profile.jsx
@@ -4,39 +4,40 @@ import axios from "axios";
 
 export default function Profile(props){
     const [file, setFile] = useState(null)
-
+    console.log(props)
     function handleSelectedFile(e){
         setFile(e.target.files[0]) // we use [] because key is a number here.
-        // console.log(e.target.files);
+        console.log(e.target.files);
+        console.log(e.target.files[0]);
     }
-
     function saveFile(e){
         // const [picture, setPicture ] = useState(null)
         if(!file){
             return alert("Select a file first:)")
-        } else {
-            const formData = new FormData()
-            formData.append("selectedFile", file)
-        
-            const config = {
-                method: "POST",
-                body: formData
-            }
-        
-            fetch(`${process.env.REACT_APP_BE_SERVER}/user/createPicture`, config)
-                .then(res => res.json())
-                .then(res =>  res.send({}))
-                .catch()
         }
+        const formData = new FormData()
+        formData.append("selectedFile", file)
+
+        const headers = { Authorization: `Bearer ${props.token}`}
+
+        const config = {
+            // data: formData,
+            headers
+        }
+        axios.post(`${process.env.REACT_APP_BE_SERVER}/picture/createPicture`, formData, config )
+            .then(result =>  props.setUserProfPic(result.data._id))
+            .catch(error => console.log(error))
+
     }
 
+    console.log("USER PROFILE from PROFILE.JSX L 34",props.userProfPic)
+//    console.log("USER PROFILE ID from PROFILE.JSX L 34",props.userProfPic.data._id)
     return(
         <article>
             PROFILE
             <input type="file" onChange={handleSelectedFile} />
             <button onClick={saveFile}>Save Picture</button>
-
-            <img src="http://localhost:9000/file/6298cca07b516dfa091e96a9" alt="Ups, no picture;)"/>
+            {props.userProfPic&&<img src={`${process.env.REACT_APP_BE_SERVER}/picture/${props.userProfPic}`} alt="Ups, no picture;)"/>}
         </article>
     )
 }
